c9c60009312bc5624acd492bb86071f64fa43364
haberman <kai@Perditas-MBP-2.fritz.box>
2020-06-12 11:14:36 +0200

    pip3 installs added

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+pip3 install -r requirements.txt 
 python3 setup_SOM.py build_ext --inplace;
 /usr/local/opt/llvm/bin/clang -fPIC -Ofast -fopenmp -shared -I/usr/local/opt/llvm/include -o libsom.so c_helper.c  -L/usr/local/opt/llvm/lib ;
 
