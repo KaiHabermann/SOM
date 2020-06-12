8745d247d95a7785c7fc02b36616dd35d0c83d7f
haberman <kai@Perditas-MBP-2.fritz.box>
2020-06-12 12:56:30 +0200

    README updated

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # SOM_for_ATLAS
+
+setup by installing git lfs<br/>
+
+then run `<em>sh setup.sh</em>` <br/>
+
+then run git pull<br/>
--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 pip3 install -r requirements.txt 
 git lfs install
 git lfs track "*.csv"
+git lfs track "csv_files/"
 python3 setup_SOM.py build_ext --inplace;
 /usr/local/opt/llvm/bin/clang -fPIC -Ofast -fopenmp -shared -I/usr/local/opt/llvm/include -o libsom.so c_helper.c  -L/usr/local/opt/llvm/lib ;
 
