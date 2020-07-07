ed2cf4c554ca052e5101dece3a4b4927d7d44e66
haberman <kai@Perditas-MBP-2.fritz.box>
2020-07-07 15:06:55 +0200

    clang support

--- setup.sh
+++ setup.sh
@@ -3,5 +3,18 @@ git lfs install
 git lfs track "*.csv"
 git lfs track "csv_files/"
 python3 setup_SOM.py build_ext --inplace;
-/usr/local/opt/llvm/bin/clang -fPIC -Ofast  -shared -I/usr/local/opt/llvm/include -o libsom.so c_helper.c  -L/usr/local/opt/llvm/lib ;
 
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+
+if (machine==Mac); then
+/usr/local/opt/llvm/bin/clang -fPIC -Ofast  -shared -I/usr/local/opt/llvm/include -o libsom.so c_helper.c  -L/usr/local/opt/llvm/lib ;
+else
+    clang -fPIC -Ofast  -shared -o libsom.so c_helper.c;
+fi
