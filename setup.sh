64eb416870a166666e50e6db5d61eaaf7286ca7c
haberman <kai@Perditas-MBP-2.fritz.box>
2020-07-07 15:49:08 +0200

    fix?

--- setup.sh
+++ setup.sh
@@ -13,7 +13,8 @@ case "${unameOut}" in
     *)          machine="UNKNOWN:${unameOut}"
 esac
 if [ "$machine" = "Mac" ] ; then
-/usr/local/opt/llvm/bin/clang -fPIC -Ofast  -shared -I/usr/local/opt/llvm/include -o libsom.so c_helper.c  -L/usr/local/opt/llvm/lib ;
+echo "Mac";
+/usr/local/opt/llvm/bin/clang -fPIC -O3  -shared -I/usr/local/opt/llvm/include -o libsom.so c_helper.c  -L/usr/local/opt/llvm/lib ;
 else
     gcc -fPIC -O3  -shared  -o -libm libsom.so c_helper.c -L/usr/local/opt/llvm/lib;
 fi
