07d24dc3c83dd4d5de77fe0cebcb1c4a08f46ff1
haberman <kai@Perditas-MBP-2.fritz.box>
2021-11-25 20:33:31 +0200

    stuff

--- setup.sh
+++ setup.sh
@@ -12,9 +12,9 @@ case "${unameOut}" in
     MINGW*)     machine=MinGw;;
     *)          machine="UNKNOWN:${unameOut}"
 esac
-if [ "$machine" = "Mac" ] ; then
-/usr/local/opt/llvm/bin/clang -fPIC -Ofast  -shared -I/usr/local/opt/llvm/include -o libsom.so c_helper.c  -L/usr/local/opt/llvm/lib ;
-else
-   gcc -fPIC -Ofast  -shared  -o libsom.so c_helper.c ;
-fi
-echo $machine;
+#if [ "$machine" = "Mac" ] ; then
+#/usr/local/opt/llvm/bin/clang -fPIC -Ofast  -shared -I/usr/local/opt/llvm/include -o libsom.so c_helper.c  -L/usr/local/opt/llvm/lib ;
+#else
+#   gcc -fPIC -Ofast  -shared  -o libsom.so c_helper.c ;
+#fi
+#echo $machine;
