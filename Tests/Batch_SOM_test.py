98c9884b694e93e4c26e8924d3b814acf3bc55b9
kai-git-stuff <kai.habermann@gmx.net>
2022-01-31 16:36:42 +0100

    Made test description clearer

--- Tests/Batch_SOM_test.py
+++ Tests/Batch_SOM_test.py
@@ -31,9 +31,10 @@ def color_test():
 	
 	# som setup
 	# for colors periodic_boundarys = False is simply prettier
-	# PCA give the option to use PCA for initial Neuron distribution
+	# PCA give the option to use PCA for initial Neuron distribution (WARNING: this can cause neurons to have values outside of [0,1], 
+	# 																	if not enough epochs are run afterwards)
 	# pool_size only important, if train_async is used
-	som = batch_SOM(map_dim,len(values[0]),values,PCA=True,periodic_boundarys=False,pool_size=4)
+	som = batch_SOM(map_dim,len(values[0]),values,PCA=True,periodic_boundarys=False,pool_size=8)
 	
 	# Training 
 	# learning_rate gives initial learning rate, while learning_rate_end gives learning rate in last epoch
@@ -46,7 +47,7 @@ def color_test():
 	# uses python, but runs each batch in prarllel
 	# only useful, if batches are large
 	# pool_size parameter give ammount of parallel threads
-	# som.train_async(prnt = False,batch_size=100,learning_rate = 0.2,sigma_end=1.,learning_rate_end = 0.01,sigma=3,radius_decrease = decrease, lr_decrease = decrease,max_epochs=500)
+	# som.train_async(prnt = False,batch_size=2000,learning_rate = 0.2,sigma_end=1.,learning_rate_end = 0.01,sigma=3,radius_decrease = decrease, lr_decrease = decrease,max_epochs=100)
 	
 	print("Training time:",time.time() - start,"s")
 
@@ -58,7 +59,6 @@ def color_test():
 	
 	# Plot
 	fig = plt.figure()
-
 	# setup axes
 	ax = fig.add_subplot(111, aspect='equal')
 	ax.set_xlim((0, map_dim[0]))
