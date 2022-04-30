1dbf29dddb0fc0a968c2ec16aa5c35a0de00f9db
Kai Habermann <kai.habermann@gmx.net>
2022-04-30 15:17:43 +0200

    setup added

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+packages = setuptools.find_packages(where='./')
+
+setuptools.setup(
+    name="SOM",
+    version="0.1",
+    packages=packages,
+    include_package_data=True,
+    description="Self-Organizing-Maps",
+    long_description=open("README.md", "r").read(),
+    package_dir={pkg: f"./{pkg.replace('.', '/')}" for pkg in packages},
+    install_requires=[line.strip() for line in open('requirements.txt', 'r').readlines()],
+    extras_require={},
+    classifiers=[],
+)
