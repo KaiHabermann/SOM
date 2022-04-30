ee54f8eaea875d6247eecb2d05fa8626603ce96d
Kai Habermann <kai.habermann@gmx.net>
2022-04-30 19:10:33 +0200

    Added so file to package

--- setup.py
+++ setup.py
@@ -12,5 +12,6 @@ setuptools.setup(
     package_dir={pkg: f"./{pkg.replace('.', '/')}" for pkg in packages},
     install_requires=[line.strip() for line in open('requirements.txt', 'r').readlines()],
     extras_require={},
+    package_data={'': ['build/libsom.so']},
     classifiers=[],
 )
